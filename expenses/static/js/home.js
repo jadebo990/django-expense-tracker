a2b094af8c6404e7f18f2889583463912375eb81
Andrea Monari <amonari1@proton.me>
2026-04-17 23:39:21 +0200

    Add expenses and income doughnut charts to homepage

--- expenses/static/js/home.js
+++ expenses/static/js/home.js
@@ -1,6 +1,111 @@
 document.addEventListener("DOMContentLoaded", async () => {
     await loadTransactions();
+    await loadCharts();
 })
+
+async function loadCharts() {
+    const response = await axios.get('/api/transactions/');
+    const transactions = response.data;
+    const categories = [];
+    transactions.forEach(t => {
+        if(t.type == 'expense') {
+            let t_category = categories.find((category) => category.name == t.category.name);
+            if(!t_category) {
+                categories.push({
+                    "type" : "expense",
+                    "name" : t.category.name,
+                    "total_amount" : parseFloat(t.amount)
+                });
+            } else {
+                t_category.total_amount += parseFloat(t.amount);
+            }
+        } else if(t.type == 'income') {
+            let t_category = categories.find((category) => category.name == t.category.name);
+            if(!t_category) {
+                categories.push({
+                    "type" : "income",
+                    "name" : t.category.name,
+                    "total_amount" : parseFloat(t.amount)
+                })
+            }else {
+                t_category.total_amount += parseFloat(t.amount);
+            }
+        }
+    })
+
+    // CHARTS
+    const ctxE = document.getElementById('expensesChart');
+    const ctxI = document.getElementById('incomeChart');
+
+    //è giusto metterle fuori le const?
+    const e_labels = [];
+    const i_labels = [];
+    const e_data = [];
+    const i_data = [];
+    const e_backgroundColor = [];
+    const i_backgroundColor = [];
+
+    categories.forEach(c => {
+        if(c.type == 'expense') {
+            e_labels.push(c.name);
+            e_data.push(c.total_amount);
+            e_backgroundColor.push(randomColor());    
+
+        } else if(c.type == 'income') {
+            i_labels.push(c.name);
+            i_data.push(c.total_amount);
+            i_backgroundColor.push(randomColor());
+        }
+    })
+
+    new Chart(ctxE, {
+        type: 'doughnut',
+        data: {
+            labels: e_labels,
+            datasets: [{
+                data: e_data,
+                backgroundColor: e_backgroundColor,
+                hoverOffset: 4,
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Total expenses',
+                }
+            }
+        }
+    });
+
+    new Chart(ctxI, {
+        type: 'doughnut',
+        data: {
+            labels: i_labels,
+            datasets: [{
+                data: i_data,
+                backgroundColor: i_backgroundColor,
+                hoverOffset: 4,
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Total income',
+                }
+            }
+        }
+    });
+    
+}
+
+function randomColor() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 async function loadTransactions() {
     const response = await axios.get('/api/transactions/');
@@ -29,3 +134,5 @@ async function loadTransactions() {
         table.append(tr);
     });
 }
+
+
