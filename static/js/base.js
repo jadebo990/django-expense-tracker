2dd768a2a687a6e8243955345ed3e542c49301b8
Andrea Monari <amonari1@proton.me>
2026-04-22 22:18:37 +0200

    Add header active link logic based on current pathname

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,16 @@
-//logica attributo active
+document.addEventListener('DOMContentLoaded', () => {
+    const pathname = window.location.pathname;
+    const nav_elements = document.querySelectorAll('.nav-link');
+    nav_elements.forEach(e => {
+        if(pathname == e.getAttribute('href')) {
+            e.classList.add('active');
+        }
+    })
+})
+
+
+/*logica spin logo: da fare
+const logo = document.getElementById('logo');
+logo.addEventListener('mouseover', () => {
+    spinLogo();
+})*/
